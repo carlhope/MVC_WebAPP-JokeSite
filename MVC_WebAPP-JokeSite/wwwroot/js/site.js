5a15b71d600a65ee0e1fd025172741095fa52e50
Carl hope <84421131+carlhope@users.noreply.github.com>
2023-08-26 16:35:46 +0100

    update

--- MVC_WebAPP-JokeSite/wwwroot/js/site.js
+++ MVC_WebAPP-JokeSite/wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+        $("#selectjoketype").change(function () {
+            if (this.value == "single") {
+                $("#oneliner").css("display", "block");
+                $("#twopartsetup").css("display", "none");
+                $("#twopartdelivery").css("display", "none");
+                $("#twopartsetupinput").val("");
+                $("#twopartdeliveryinput").val("");
+            }
+            else {
+                $("#oneliner").css("display", "none");
+                $("#twopartsetup").css("display", "block");
+                $("#twopartdelivery").css("display", "block");
+                $("#onelinerinput").val("");
+            }
+        });
